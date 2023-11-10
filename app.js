15067dbcb82e8eff3215ca0777086bd9df315078
BhargavUPPU <uppubhargavsai2013@gmail.com>
2023-11-11 00:07:43 +0530

    Task manger

---
+++ app.js
@@ -0,0 +1,22 @@
+const connectDB=require('./db/connect')
+require('dotenv').config();
+const await=require('await')
+const express=require('express');
+const  app=express();
+const tasks=require('./rotues/tasks');
+//middle ware
+app.use(express.static('./public'));
+app.use(express.json())
+//routes
+ app.use('/api/v1/task',tasks)
+const port=5000
+const start=async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is listening on port ${port}`))
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+start();
